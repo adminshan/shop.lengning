a904a663d3f4563f4821f229cb24cee755a3c412
adminshan <2644586064@qq.com>
2019-01-15 13:45:48 +0800

    debug

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,9 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         '/test/*',
-        '/user/*',
-        '/reg',
-        '/login',
         '/pay/alipay/notify',
     ];
 }
