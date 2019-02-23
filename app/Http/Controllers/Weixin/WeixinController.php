929273be50df313c4d93b32fc71742c9ec402ed2
adminshan <2644586064@qq.com>
2019-02-23 10:53:20 +0800

    dd

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -308,51 +308,50 @@ class WeixinController extends Controller
         $client = new GuzzleHttp\Client(['base_uri' => $url]);
 
         $data = [
-            "button"=>[
-            [
-            "name"=> "扫码",
-            "sub_button"=>[
+            "button"    => [
                 [
-                    "type"=> "scancode_waitmsg",
-                    "name"=> "扫码带提示",
-                    "key"=> "rselfmenu_0_0",
-                    "sub_button"=> [ ]
+                    'name'=>'连接',
+                    "sub_button"=>[
+                        [
+                            "type"  => "view",      // view类型 跳转指定 URL
+                            "name"  => "百度一下",
+                            "url"   => "https://www.baidu.com"
+                        ],
+                        [
+                            "type"  => "view",      // view类型 跳转指定 URL
+                            "name"  => "淘宝",
+                            "url"   => "https://www.baidu.com"
+                        ],
+                    ]
                 ],
-                [
-                    "type"=> "scancode_push",
-                    "name"=> "扫码推事件",
-                    "key"=> "rselfmenu_0_1",
-                    "sub_button"=> [ ]
-                ]
-            ]
-        ],
-        [
 
-            "name"=> "发图",
-            "sub_button"=> [
                 [
-                    "type"=> "pic_sysphoto",
-                    "name"=> "系统拍照发图",
-                    "key"=> "rselfmenu_1_0",
-                 ],
-                [
-                    "type"=> "pic_photo_or_album",
-                    "name"=> "拍照或者相册发图",
-                    "key"=> "rselfmenu_1_1",
+                    "type"  => "click",      // click类型
+                    "name"  => "客服",
+                    "key"   => "kefu01"
                 ],
-            ]
-        ],
-        [
-            "name"=> "发送位置",
-            "type"=> "location_select",
-            "key"=> "rselfmenu_2_0"
-        ],
-        [
-            "type"=> "media_id",
-           "name"=> "图片",
-           "media_id"=> "MEDIA_ID1"
-        ],
-        ]
+                [
+                    "name"=> "发图",
+                    "sub_button"=> [
+                        [
+                            "type"=> "pic_sysphoto",
+                            "name"=> "系统拍照发图",
+                            "key"=> "rselfmenu_1_0",
+
+                         ],
+                        [
+                            "type"=> "pic_photo_or_album",
+                            "name"=> "拍照或者相册发图",
+                            "key"=> "rselfmenu_1_1",
+                        ],
+                        [
+                            "type"=> "pic_weixin",
+                            "name"=> "微信相册发图",
+                            "key"=> "rselfmenu_1_2",
+                        ]
+                    ]
+                ],
+            ],
         ];
 
 
