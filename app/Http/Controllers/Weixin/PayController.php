493766c9948c062575c1340f0bfbf280216959c3
adminshan <2644586064@qq.com>
2019-02-27 11:38:28 +0800

    zhifu

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -42,7 +42,7 @@ class PayController extends Controller
 
    }
     public function success(){
-        $order_sn=$_POST['prder_sn'];
+        $order_sn=$_POST['order_sn'];
         $data=OrderModel::where(['order_sn'=>$order_sn])->first()-toArray();
         if($data['status'==3]){
             echo 1;
