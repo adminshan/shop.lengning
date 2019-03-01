0ea44f9eaf3622133e91f8dd063f58d07ecad64b
adminshan <2644586064@qq.com>
2019-03-01 13:50:20 +0800

    jssdk

--- resources/views/layouts/bst.blade.php
+++ resources/views/layouts/bst.blade.php
@@ -1,11 +1,11 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>BootStrap</title>
-
     <link rel="stylesheet" href="{{URL::asset('/bootstrap/css/bootstrap.min.css')}}">
 </head>
 <body>
@@ -16,6 +16,10 @@
     <script src="{{URL::asset('/js/jquery-1.12.4.min.js')}}"></script>
     <script src="{{URL::asset('/bootstrap/js/bootstrap.min.js')}}"></script>
 
-   @show
+@show
 </body>
 </html>
+
+
+
+
